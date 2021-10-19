b738bd1d9b5702b6904c389b8ba9ccea6daa7b04
Luis G <lglaser@cs.uni-frankfurt.de>
2021-10-19 16:43:16 +0200

    restructure wrapper

--- src/viasp/wrapper.py
+++ src/viasp/wrapper.py
@@ -1,5 +1,6 @@
 import json
 from inspect import signature
+from typing import List
 
 import requests
 from clingo import Control as InnerControl
@@ -8,7 +9,7 @@ from dataclasses import asdict, is_dataclass
 from .clingoApiClient import ClingoClient
 from .server.database import ClingoMethodCall
 from .shared.model import Model
-from .shared.simple_logging import Level, log, warn
+from .shared.simple_logging import warn
 
 
 def backend_is_running():
@@ -25,44 +26,51 @@ def is_non_cython_function_call(attr: classmethod):
 
 class PaintConnector:
 
+    def __init__(self, **kwargs):
+        self._marked: List[Model] = []
+        self._database = ClingoClient()
+        self._connection = None
+
     def paint(self):
         if not backend_is_running():
             raise Exception("Server is not available")
+        # elif self._connection is None:
+        #     self._connection = _make_connection()
+        # self._connection.
 
     def unmark(self, model: Model):
-        raise NotImplementedError
+        self._marked.remove(model)
 
     def mark(self, model: Model):
-        raise NotImplementedError
+        self._marked.append(model)
 
     def clear(self):
-        raise NotImplementedError
+        self._marked.clear()
+
+    def register_function_call(self, name, sig, args, kwargs):
+        serializable_call = ClingoMethodCall.merge(name, sig, args, kwargs)
+        self._database.save_function_call(serializable_call)
+        print(f"Registered {serializable_call}")
 
 
 class Control(InnerControl):
 
-    def _register_function_call(self, name, sig, args, kwargs):
-        serializable_call = ClingoMethodCall.merge(name, sig, args, kwargs)
-        self.database.save_function_call(serializable_call)
-        print(f"Registered {serializable_call}")
-
     def __init__(self, *args, **kwargs):
-        self.viasp = PaintConnector()
-        self.database = ClingoClient(**kwargs)
+        self.viasp = PaintConnector(**kwargs)
         if not backend_is_running():
             warn("You are using the vizgo control object and no server is running right now")
             # app = factory.create_app()
             # from waitress import serve
             # serve(app, host="0.0.0.0", port=8080)
             # TODO: output good warning
-        self._register_function_call("__init__", signature(super().__init__), args, kwargs)
+        self.viasp.register_function_call("__init__", signature(super().__init__), args, kwargs)
         super().__init__(*args, **kwargs)
 
     def __getattribute__(self, name):
         attr = InnerControl.__getattribute__(self, name)
         if is_non_cython_function_call(attr):
             def wrapper_func(*args, **kwargs):
-                self._register_function_call(attr.__name__, signature(attr), args, kwargs)
+                self.viasp.register_function_call(attr.__name__, signature(attr), args, kwargs)
                 result = attr(*args, **kwargs)
                 return result
 
