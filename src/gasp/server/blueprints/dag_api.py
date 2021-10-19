7cad02fd07796a39ce444871dc15540082cd9619
Luis G <lglaser@cs.uni-frankfurt.de>
2021-10-19 16:07:11 +0200

    add sample data to api

--- src/gasp/server/blueprints/dag_api.py
+++ src/gasp/server/blueprints/dag_api.py
@@ -1,6 +1,23 @@
+import json
+
+import networkx as nx
 from flask import Blueprint
 
+from gasp.shared.io import DataclassJSONDecoder, DataclassJSONEncoder
+
 bp = Blueprint("dag_api", __name__, template_folder='server/templates')
+
+
+def dumb_fuck() -> nx.Graph:
+    result = None
+    with open("/Users/bianchignocchi/Developer/cogsys/0_ma/gasp/tests/resources/traveling_salesperson.json",
+              encoding="utf-8") as f:
+        result = json.load(f, cls=DataclassJSONDecoder)
+    loaded_graph = nx.node_link_graph(result)
+    return loaded_graph
+
+
+DUMB = dumb_fuck()
 
 
 @bp.route("/model/<uuid>", methods=["GET"])
@@ -11,6 +28,13 @@ def get_model(uuid):
 @bp.route("/rule/<uuid>", methods=["GET"])
 def get_rule(uuid):
     return NotImplementedError
+
+
+@bp.route("/rules", methods=["GET"])
+def get_all_rules():
+    to_return = list(DUMB.nodes)
+    to_return = json.dumps(to_return, cls=DataclassJSONEncoder)
+    return to_return
 
 
 @bp.route("/graph/", methods=["GET"])
