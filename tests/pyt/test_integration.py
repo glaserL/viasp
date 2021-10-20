260e448031624c4183db8ac142b65f2200b51287
Luis G <lglaser@cs.uni-frankfurt.de>
2021-10-20 13:23:19 +0200

    Setup full api for integration tests

--- tests/pyt/test_integration.py
+++ tests/pyt/test_integration.py
@@ -1,5 +1,15 @@
+import multiprocessing
+import time
+
+import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+
 from src.viasp.server.database import CallCenter
 from src.viasp.asp.replayer import apply_multiple
+from src.viasp.server.blueprints.api import bp as api_bp
+from src.viasp.server.blueprints.app import bp as app_bp
+from src.viasp.server.blueprints.dag_api import bp as dag_bp
 from tests.pyt.test_replayer import run_sample
 
 
@@ -11,3 +21,24 @@ def test_calls_are_filtered_after_application():
     _ = apply_multiple(db.get_all())
     assert len(db.get_all()) == 4, "Get all should still return all of them after application."
     assert len(db.get_pending()) == 0, "The call objects should be marked as used after application."
+
+
+def create_app_with_registered_blueprints(*bps) -> Flask:
+    app = Flask(__name__)
+    for bp in bps:
+        app.register_blueprint(bp)
+
+    return app
+
+
+@pytest.fixture
+def client() -> FlaskClient:
+    app = create_app_with_registered_blueprints(app_bp, api_bp, dag_bp)
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_client_works(client):
+    """Test if the test client is ok"""
+    assert client.get("/").status_code == 200
