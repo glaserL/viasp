1e62ac02483c1267054f06c15a3b732fc6020e35
Luis G <lglaser@cs.uni-frankfurt.de>
2021-10-26 17:00:17 +0200

    Prototypical state management in API to store graph

--- src/viasp/server/blueprints/dag_api.py
+++ src/viasp/server/blueprints/dag_api.py
@@ -1,23 +1,41 @@
 import json
+from typing import Union
 
 import networkx as nx
-from flask import Blueprint
+from flask import Blueprint, request, render_template, Response, make_response, jsonify
 
 from ...shared.io import DataclassJSONDecoder, DataclassJSONEncoder
 
 bp = Blueprint("dag_api", __name__, template_folder='server/templates')
 
 
-def dumb_fuck() -> nx.Graph:
-    result = None
-    with open("/Users/bianchignocchi/Developer/cogsys/0_ma/viasp/tests/resources/traveling_salesperson.json",
-              encoding="utf-8") as f:
-        result = json.load(f, cls=DataclassJSONDecoder)
-    loaded_graph = nx.node_link_graph(result)
-    return loaded_graph
+class GraphDataBaseKEKL:
+
+    def __init__(self):
+        self.path = "/Users/bianchignocchi/Developer/cogsys/0_ma/gasp/src/viasp/server/graph.json"
+
+    def save(self, graph: Union[nx.Graph, dict]):
+        if isinstance(graph, nx.Graph):
+            serializable_graph = nx.node_link_data(graph)
+        else:
+            serializable_graph = graph
+        with open(self.path, "w", encoding="utf-8") as f:
+            json.dump(serializable_graph, f, cls=DataclassJSONEncoder, ensure_ascii=False, indent=2)
+
+    def load(self, as_json=True) -> Union[nx.Graph, dict]:
+        try:
+            with open(self.path, encoding="utf-8") as f:
+                result = json.load(f, cls=DataclassJSONDecoder)
+            if as_json:
+                return result
+            loaded_graph = nx.node_link_graph(result)
+            return loaded_graph
+        except FileNotFoundError:
+            return nx.DiGraph()
 
 
-DUMB = nx.Graph()
+def get_database():
+    return GraphDataBaseKEKL()
 
 
 @bp.route("/model/<uuid>", methods=["GET"])
@@ -32,11 +50,16 @@ def get_rule(uuid):
 
 @bp.route("/rules", methods=["GET"])
 def get_all_rules():
-    to_return = list(DUMB.nodes)
-    to_return = json.dumps(to_return, cls=DataclassJSONEncoder)
-    return to_return
+    r = jsonify(list(get_database().load(as_json=False).edges))
+    return r
 
 
-@bp.route("/graph/", methods=["GET"])
-def get_graph():
-    return NotImplementedError
+@bp.route("/graph", methods=["POST", "GET"])
+def graph():
+    if request.method == "POST":
+        data = request.json
+        print(f"Saving {data}")
+        get_database().save(data)
+        return "ok"
+    elif request.method == "GET":
+        return get_database().load()
