feeff3b8a6d3e494db085ac48f1e3cb27928f3e5
Luis G <lglaser@cs.uni-frankfurt.de>
2021-10-27 19:03:24 +0200

    fix

--- tests/pyt/internal_test.py
+++ tests/pyt/internal_test.py
@@ -1,5 +1,6 @@
 import json
 
+import pytest
 import requests
 from networkx import node_link_data
 
@@ -9,11 +10,14 @@ from tests.pyt.test_graph_creation import get_stable_models_for_program
 from src.viasp.shared.io import DataclassJSONEncoder
 
 
-def test_my_ass():
+@pytest.mark.skip(reason="Not for pipeline.")
+def test_writing_to_server():
     program = "c(1). c(2). b(X) :- c(X). a(X) :- b(X)."
     transformed = transform(program)
     saved_models = get_stable_models_for_program(program)
     g = build_graph(saved_models, transformed, program)
+    assert len(g.nodes) == 3
+    assert len(g.edges) == 2
 
     backend_url = "http://127.0.0.1:5000/"
     serializable_graph = node_link_data(g)
