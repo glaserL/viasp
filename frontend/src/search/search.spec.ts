24da0ea8470670570d9d8190e1174007c6f46855
Luis G <lglaser@cs.uni-frankfurt.de>
2021-11-16 11:37:37 +0100

    Testing with instant mocha

---
+++ frontend/src/search/search.spec.ts
@@ -0,0 +1,10 @@
+var assert = require('assert');
+import {initializeSearchBar} from './search';
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
