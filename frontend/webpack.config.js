91260e59cb7672d0a8194c5f88f4a126f0aa3c6c
Luis G <lglaser@cs.uni-frankfurt.de>
2021-12-06 09:16:03 +0100

    setup webpack electron config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,18 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// {
+//   entry: path.join(__dirname, "src", "js", "main.js"),
+//   output: {
+//     path: path.join(__dirname, "build"),
+//     filename: "main.js"
+//   },
+//   target: "electron-main",
+// },
+// {
+//   entry: path.join(__dirname, "src", "js", "renderer.js"),
+//   output: {
+//     path: path.join(__dirname, "build"),
+//     filename: "renderer.js"
+//   },
+//   target: "electron-renderer"
+// }
 
-
-module.exports = {
+module.exports = [{
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
     },
-    entry: {
-        index: './src/index.ts',
-    },
+    entry: './src/main.ts',
+    target: 'electron-main',
     output: {
-        filename: '[name].bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
@@ -20,7 +34,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'main.html',
             template: 'src/main.html',
-        }),
+        })
     ],
     module: {
         rules: [{
@@ -38,5 +52,41 @@ module.exports = {
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
+    }
+}, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
     },
-};
+    entry: './src/renderer.ts',
+    target: 'electron-renderer',
+    output: {
+        filename: 'renderer.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'main.html',
+            template: 'src/main.html',
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.(tsx|ts)?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }, {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    }
+}];
