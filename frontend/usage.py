af5ece7cbb8545dc905d8757bfd7d81b322fb9fe
Luis G <lglaser@cs.uni-frankfurt.de>
2021-12-21 20:29:55 +0100

    Add clutter from cookiecutter

--- frontend/usage.py
+++ frontend/usage.py
@@ -1,6 +1,6 @@
 import viasp_dash
 import dash
-from dash.dependencies import Input, Output, ALL
+from dash.dependencies import Input, Output
 import dash_html_components as html
 
 app = dash.Dash(__name__)
@@ -13,16 +13,6 @@ app.layout = html.Div([
 ])
 
 
-#
-#
-# @app.callback(Output('output', 'children'), [Input({'type': 'node', 'id': ALL}, 'value')])
-# def display_output(value):
-#     return f"You clicked on {value}"
-#
-#
-
-# @app.callback(Output('output', 'children'), Input("FUCKTHIS", 'rule'))
-# @app.callback(Output('output', 'children'), [Input("row_1", 'value')])
 @app.callback(Output('output', 'children'), Input('myID', 'node'))
 def display_output(rule):
     return f"I come from python, you clicked on {rule}"
