91892c5153c09ddad45b71cff9ecc88a31de6ac2
Luis G <lglaser@cs.uni-frankfurt.de>
2022-01-09 14:13:42 +0100

    Reimplement facts and rows as functions with hooks

--- frontend/src/lib/components/Facts.react.js
+++ frontend/src/lib/components/Facts.react.js
@@ -1,43 +1,45 @@
 import {backendURL} from "../utils/index";
 import {RowHeader} from "./RowHeader.react";
 import {Node} from "./Node.react";
-import React from "react";
-import {Row} from "./Row.react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 
-export class Facts extends Row {
+function loadFacts() {
+    return fetch(`${backendURL("facts")}`).then(r => r.json());
+}
 
-    constructor(props, context) {
-        super(props, context);
-    }
+export function Facts(props) {
 
-    loadMyAsyncData() {
-        return fetch(`${backendURL("facts")}`).then(r => r.json()).catch(error => this.setState({error}))
-    }
+    const {notifyClick} = props;
 
-    render() {
-        const {notifyClick} = this.props;
-        if (this.state.externalData === null) {
-            return (
-                <div className="row_container">
-                    <RowHeader rule={["<Facts>"]}/>
-                    <div>Loading Transformations..</div>
-                </div>
-            )
-        }
-        if (this.state.show === false) {
-            return <div className="row_container">
-                <RowHeader onToggle={this.onHeaderClick} rule={["<Facts>"]}/>
+    const [fact, setFact] = useState(null);
+    const [hideNodes, setHideNodes] = useState(false);
+
+    useEffect(() => {
+        let mounted = true;
+        loadFacts()
+            .then(items => {
+                if (mounted) {
+                    setFact(items)
+                }
+            })
+        return () => mounted = false;
+    }, []);
+    if (fact === null) {
+        return (
+            <div className="row_container">
+                <RowHeader rule={["<Facts>"]}/>
+                <div>Loading Transformations..</div>
             </div>
-        }
-        const fact = this.state.externalData
-        return <div className="row_container">
-            <RowHeader onToggle={this.onHeaderClick} rule={["<Facts>"]}/>
-            <div className="row_row"><Node key={fact.uuid} id={fact}
-                                           notifyClick={notifyClick}/>
-            </div>
-        </div>
+        )
     }
+
+    return <div className="row_container">
+        <RowHeader onToggle={() => setHideNodes(!hideNodes)} rule={["<Facts>"]}/>
+        {hideNodes ? null : <div className="row_row"><Node key={fact.uuid} id={fact}
+                                                           notifyClick={notifyClick}/>
+        </div>}
+    </div>
 }
 
 Facts.propTypes = {
