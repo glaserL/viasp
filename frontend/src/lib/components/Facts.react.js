8681f6fa1f1b2be8fb4e981fb35216e4ea84d77f
Luis G <lglaser@cs.uni-frankfurt.de>
2022-01-15 00:07:11 +0100

    Parameterize colors and more responsiveness

--- frontend/src/lib/components/Facts.react.js
+++ frontend/src/lib/components/Facts.react.js
@@ -1,8 +1,9 @@
 import {backendURL} from "../utils/index";
-import {RowHeader} from "./RowHeader.react";
-import {Node} from "./Node.react";
+import "./facts.css";
 import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
+import {hideNode, showNode, useShownNodes} from "../contexts/ShownNodes";
+import {useColorPalette} from "../contexts/ColorPalette";
 
 function loadFacts() {
     return fetch(`${backendURL("facts")}`).then(r => r.json());
@@ -13,8 +14,6 @@ export function Facts(props) {
     const {notifyClick} = props;
 
     const [fact, setFact] = useState(null);
-    const [hideNodes, setHideNodes] = useState(false);
-
     useEffect(() => {
         let mounted = true;
         loadFacts()
@@ -28,24 +27,51 @@ export function Facts(props) {
     if (fact === null) {
         return (
             <div className="row_container">
-                <RowHeader rule={["<Facts>"]}/>
-                <div>Loading Transformations..</div>
+                <div>Loading facts..</div>
             </div>
         )
     }
-
-    return <div className="row_container">
-        <RowHeader onToggle={() => setHideNodes(!hideNodes)} rule={["<Facts>"]}/>
-        {hideNodes ? null : <div className="row_row"><Node key={fact.uuid} node={fact}
-                                                           notifyClick={notifyClick}/>
-        </div>}
-    </div>
+    //
+    // return <div className="row_container">
+    //     <RowHeader onToggle={() => setHideNodes(!hideNodes)} rule={["<Facts>"]}/>
+    //     {hideNodes ? null : <div className="row_row"><Node key={fact.uuid} node={fact}
+    //                                                        notifyClick={notifyClick}/>
+    //     </div>}
+    // </div>
+    //
+    return <ActualFactThingy fact={fact}/>
 }
 
 Facts.propTypes = {
+    notify: PropTypes.func
+}
+
+function ActualFactThingy(props) {
+    const {fact} = props;
+    const [, dispatch] = useShownNodes()
+    const colorPalette = useColorPalette();
+
+    useEffect(() => {
+        dispatch(showNode(fact.uuid))
+        return () => {
+            dispatch(hideNode(fact.uuid))
+        }
+    }, [])
+    const clazzName = `${fact.uuid} facts_banner noselect`
+    return <div className={clazzName}
+                style={{"color": colorPalette.sixty, "background-color": colorPalette.ten}}>Facts</div>
+}
+
+ActualFactThingy.propTypes = {
     /**
      * The ID used to identify this component in Dash callbacks.
      */
-    id: PropTypes.string
-
+    id: PropTypes.string,
+    fact: PropTypes.exact({
+        diff: PropTypes.array,
+        atoms: PropTypes.array,
+        uuid: PropTypes.string,
+        _type: PropTypes.string,
+        rule_nr: PropTypes.number
+    }),
 }
