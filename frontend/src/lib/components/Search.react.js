eaf530bf9439c5b794b6040871321d280a3258ca
Luis G <lglaser@cs.uni-frankfurt.de>
2022-01-16 11:29:53 +0100

    Make search a functional component

--- frontend/src/lib/components/Search.react.js
+++ frontend/src/lib/components/Search.react.js
@@ -1,74 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {backendURL} from "../utils/index";
 import "./search.css";
 import {SearchResult} from "./SearchResult.react";
-import PropTypes from "prop-types";
-
-export class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // The active selection's index
-            activeSuggestion: 0,
-            // The suggestions that match the user's input
-            filteredSuggestions: [],
-            // Whether or not the suggestion list is shown
-            showSuggestions: false,
-            // What the user has entered
-            userInput: ""
-        };
-    }
-
-    render() {
-        const {
-            onChange,
-            onClick,
-            onKeyDown,
-            state: {
-                activeSuggestion,
-                filteredSuggestions,
-                showSuggestions,
-                userInput
-            }
-        } = this;
-
-        let suggestionsListComponent;
-        console.log(`I have ${filteredSuggestions.length} suggestions to display`)
-
-        if (showSuggestions && userInput) {
-            if (filteredSuggestions.length) {
-                suggestionsListComponent = (
-                    <ul className="search_result_list">
-                        {filteredSuggestions.map((suggestion, index) => {
-                            // Flag the active suggestion with a class
-                            return <SearchResult active={index === activeSuggestion} key={index}
-                                                 value={suggestion} onClick={onClick}/>
-                        })}
-                    </ul>
-                );
-            } else {
-                suggestionsListComponent = (
-                    <div className="no-suggestions">
-                        <em>No suggestions, you're on your own!</em>
-                    </div>
-                );
-            }
-        }
-        return (
-            <div className="search">
-                {suggestionsListComponent}
-                <input
-                    type="text"
-                    onChange={onChange}
-                    onKeyDown={onKeyDown}
-                    value={userInput}
-                />
-            </div>
-        );
-    }
+import PropTypes, {func} from "prop-types";
 
 
-    onChange = e => {
+// https://blog.logrocket.com/build-react-autocomplete-component/
+
+
+export function Search(props) {
+    const [activeSuggestion, setActiveSuggestion] = useState(0);
+    const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+    const [userInput, setUserInput] = useState("");
+
+    let suggestionsListComponent;
+
+    function onChange(e) {
         const userInput = e.currentTarget.value;
 
         // Filter our suggestions that don't contain the user's input
@@ -76,61 +24,81 @@ export class Search extends Component {
         fetch(`${backendURL("query")}?q=` + userInput)
             .then(r => r.json())
             .then(data => {
-                console.log("LOOK AT ME")
-                this.setState({
-                    activeSuggestion: 0,
-                    filteredSuggestions: data,
-                    showSuggestions: true,
-                    userInput: userInput
-                })
+                setActiveSuggestion(0)
+                setFilteredSuggestions(data)
+                setShowSuggestions(true)
+                setUserInput(userInput)
                 console.log(`Saved ${data.length} suggestions.`)
             })
-    };
+    }
 
-    onClick = e => {
-        this.setState({
-            activeSuggestion: 0,
-            filteredSuggestions: [],
-            showSuggestions: false,
-            userInput: e.currentTarget.innerText
-        });
-    };
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
 
-    onKeyDown = e => {
-        const {activeSuggestion, filteredSuggestions} = this.state;
+    function onClick(e) {
+        setActiveSuggestion(0)
+        setFilteredSuggestions([])
+        setShowSuggestions(false)
+        setUserInput(e.currentTarget.innerText)
+    }
+
+    function onKeyDown(e) {
 
         // User pressed the enter key
         if (e.keyCode === 13) {
-            this.setState({
-                activeSuggestion: 0,
-                showSuggestions: false,
-                userInput: filteredSuggestions[activeSuggestion]
-            });
+            setActiveSuggestion(0);
+            setShowSuggestions(false);
+            setUserInput("");
         }
         // User pressed the up arrow
         else if (e.keyCode === 38) {
+
             if (activeSuggestion === 0) {
                 return;
             }
-
-            this.setState({activeSuggestion: activeSuggestion - 1});
+            setActiveSuggestion(activeSuggestion - 1);
         }
         // User pressed the down arrow
         else if (e.keyCode === 40) {
             if (activeSuggestion - 1 === filteredSuggestions.length) {
                 return;
             }
-
-            this.setState({activeSuggestion: activeSuggestion + 1});
+            setActiveSuggestion(activeSuggestion + 1);
         }
-    };
-
-    handleSubmit(event) {
-        event.preventDefault();
     }
 
-
+    if (showSuggestions && userInput) {
+        if (filteredSuggestions.length) {
+            suggestionsListComponent = (
+                <ul className="search_result_list">
+                    {filteredSuggestions.map((suggestion, index) => {
+                        return <SearchResult active={index === activeSuggestion} key={index}
+                                             value={suggestion} onClick={onClick}/>
+                    })}
+                </ul>
+            );
+        } else {
+            suggestionsListComponent = (
+                <div className="no-suggestions">
+                    <em>No suggestions, you're on your own!</em>
+                </div>
+            );
+        }
+    }
+    return (
+        <div className="search">
+            {suggestionsListComponent}
+            <input
+                type="text"
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                value={userInput}
+            />
+        </div>
+    );
 }
+
 
 Search.propTypes = {
     /**
