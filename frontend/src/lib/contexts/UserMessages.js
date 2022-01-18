13ed1317bb28c642808c6562f62560cda11440a5
Luis G <lglaser@cs.uni-frankfurt.de>
2022-01-18 09:51:40 +0100

    Create error message list

---
+++ frontend/src/lib/contexts/UserMessages.js
@@ -0,0 +1,35 @@
+import React, {createContext, useContext, useReducer} from "react";
+import PropTypes from "prop-types";
+
+export const initialState = {activeMessages: []}
+export const ERROR = 'APP/MESSAGES/ERROR';
+export const WARN = 'APP/MESSAGES/WARN';
+export const showError = (message) => ({type: ERROR, text: message})
+export const showWarn = (message) => ({type: WARN, text: message})
+export const messageReducer = (state = initialState, action) => {
+    console.log(`Reducing ${JSON.stringify(state)}, ${JSON.stringify(action)}`)
+    if (action.type === ERROR) {
+        console.log("KIKI DO YOU LOVE ME")
+        return {
+            ...state,
+            activeMessages: state.activeMessages.concat({text: action.text, level: "error"})
+        }
+    }
+    if (action.type === WARN) {
+        return {
+            ...state,
+            activeMessages: state.activeMessages.concat({text: action.text, level: "warn"})
+        }
+    }
+    return {...state}
+}
+const UserMessagesContext = createContext([]);
+export const useMessages = () => useContext(UserMessagesContext);
+export const UserMessagesProvider = ({children}) => {
+    const [state, dispatch] = useReducer(messageReducer, initialState);
+    return <UserMessagesContext.Provider value={[state, dispatch]}>{children}</UserMessagesContext.Provider>
+}
+
+UserMessagesProvider.propTypes = {
+    children: PropTypes.element
+}
