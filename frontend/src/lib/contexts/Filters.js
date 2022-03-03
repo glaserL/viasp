0f51a8e45e1636f73701fceaa04b365eae9ee540
Luis G <lglaser@cs.uni-frankfurt.de>
2022-03-03 20:44:10 +0100

    Update filters context

--- frontend/src/lib/contexts/Filters.js
+++ frontend/src/lib/contexts/Filters.js
@@ -3,18 +3,26 @@ import React, {createContext, useContext, useReducer} from "react";
 
 export const initialState = {activeFilters: []}
 export const ADD_SIGNATURE = 'FILTERS/SIGNATURE/ADD';
-export const CLEAR_SIGNATURE = 'FILTERS/SIGNATURE/CLEAR';
+export const REMOVE_SIGNATURE = 'FILTERS/SIGNATURE/CLEAR';
 export const ADD_MODEL = 'FILTERS/MODEL/ADD';
 export const CLEAR_MODEL = 'FILTERS/MODEL/CLEAR';
+export const REMOVE = 'FILTERS/REMOVE';
 
 export const addSignature = (signature) => ({type: ADD_SIGNATURE, signature: signature})
-export const clearSignature = () => ({type: CLEAR_SIGNATURE})
+export const clearSignature = () => ({type: REMOVE_SIGNATURE})
+const clear = (filter) => ({type: REMOVE, filter: filter})
 export const filterReducer = (state = initialState, action) => {
     console.log(`Reducing ${JSON.stringify(state)}, ${JSON.stringify(action)}`)
     if (action.type === ADD_SIGNATURE) {
         return {
             ...state,
             activeFilters: state.activeFilters.concat(action.signature)
+        }
+    }
+    if (action.type === REMOVE) {
+        return {
+            ...state,
+            activeFilters: state.activeFilters.filter(filter => filter !== action.filter)
         }
     }
     return {...state}
@@ -29,3 +37,5 @@ export const FilterProvider = ({children}) => {
 FilterProvider.propTypes = {
     children: PropTypes.element
 }
+
+export {clear}
