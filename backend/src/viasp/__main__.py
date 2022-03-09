16b9668e55aecc736f6c1bfb335a4f07e80a0572
Luis G <lglaser@cs.uni-frankfurt.de>
2022-03-09 15:44:27 +0100

    Parameterize backendURL in backend too

--- backend/src/viasp/__main__.py
+++ backend/src/viasp/__main__.py
@@ -1,10 +1,18 @@
-from viasp.shared.config import BACKEND_HOST, BACKEND_PORT
+import argparse
+
+from viasp.shared.defaults import DEFAULT_BACKEND_HOST, DEFAULT_BACKEND_PORT
 
 
 def start():
     from viasp.server.factory import create_app
+    parser = argparse.ArgumentParser(description='viasp backend')
+    parser.add_argument('--host', type=str, help='The host for the backend', default=DEFAULT_BACKEND_HOST)
+    parser.add_argument('-p', '--port', type=int, help='The port for the backend', default=DEFAULT_BACKEND_PORT)
     app = create_app()
     use_reloader = False
     debug = False
-    print(f"Starting viASP backend at {BACKEND_HOST}:{BACKEND_PORT}")
-    app.run(host=BACKEND_HOST, port=BACKEND_PORT, use_reloader=use_reloader, debug=debug)
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+    print(f"Starting viASP backend at {host}:{port}")
+    app.run(host=host, port=port, use_reloader=use_reloader, debug=debug)
