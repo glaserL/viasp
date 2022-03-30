897c6c01ab7c644143ffe9538ecc046e9d31ec41
Luis G <lglaser@cs.uni-frankfurt.de>
2022-03-30 13:17:22 +0200

    Update quickstart

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -1,26 +1,35 @@
+import sys
+
 import viasp_dash
 from dash import Dash
 from viasp import Control
 
-program = """
-{rain; sprinkler} 1.
-wet:- rain.
-wet:- sprinkler.
-"""
-ctl = Control(["0"])
-ctl.add("base", [], program)
-ctl.ground([("base", [])])
-with ctl.solve(yield_=True) as handle:
-    for model in handle:
-        ctl.viasp.mark(model)
-ctl.viasp.show()
+
+def main():
+    options = ["0"]
+
+    ctl = Control(options, viasp_backend_url="http://localhost:5050")
+    for path in sys.argv[1:]:
+        ctl.load(path)
+    if not sys.argv[1:]:
+        ctl.load("-")
+    ctl.ground([("base", [])])
+
+    with ctl.solve(yield_=True) as handle:
+        for m in handle:
+            print("Answer:\n{}".format(m))
+            ctl.viasp.mark(m)
+        print(handle.get())
+    ctl.viasp.show()
+
 
 app = Dash(__name__)
 
 app.layout = viasp_dash.ViaspDash(
     id="myID",
-    backendURL="http://localhost:5000"
+    backendURL="http://localhost:5050"
 )
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    main()
+    app.run_server()
